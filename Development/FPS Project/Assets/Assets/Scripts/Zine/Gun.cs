285163f6bc3d92ae752fec0e6fe37833375003a3
Zinedine038 <zsghari@hotmail.com>
2016-02-03 17:14:08 +0100

    Gun V1
    Auto Semi Bolt Action

--- Development/FPS Project/Assets/Assets/Scripts/Zine/Gun.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Zine/Gun.cs
@@ -15,65 +15,139 @@ public class Gun : MonoBehaviour
     public FireType fireType;
     public float roundsPerMinute;
     public float timeBetweenShots;
-    private float fireRate;
-    private bool  mayFire = true;
+    public AudioClip pumpSound;
+    public float fireRate;
+    public float pumpTime;
+    public int ammoInClip;
+    public int fullAmmoInClip;
+    public float reloadSpeed;
+    private bool mayFire = true;
+    public AudioClip emptySound;
+    public AudioClip shot;
+    public AudioSource audioSource;
+    public AudioClip reloadSound;
     // Use this for initialization
     void Start()
     {
-        fireRate=60/roundsPerMinute;
-        print (fireRate);
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
+            fireRate = 60 / roundsPerMinute;
+            print(fireRate);
             Fire();
+        }
+        if(Input.GetButtonDown("Reload") && ammoInClip!=fullAmmoInClip)
+        {
+            StopAllCoroutines();
+            StartCoroutine("Reload");
         }
     }
 
     void Fire()
     {
-        switch(fireType)
+        if (ammoInClip >= 1)
         {
-            case FireType.SemiAutomatic:
-                if(Input.GetButtonDown("Fire1"))
-                {
-                    FireBullet();
-                }
-                break;
-            case FireType.Automatic:
-                if(mayFire==true)
-                {
-                    FireBullet();
-                    StartCoroutine("Cooldown");
-                }
-                if(Input.GetButtonUp("Fire1"))
-                {
-                    StopCoroutine("Cooldown");
-                    mayFire=true;
-                }
-                break;
-            case FireType.BoltOrPumpAction:
-                break;
+            switch (fireType)
+            {
+                case FireType.SemiAutomatic:
+                    if (Input.GetButtonDown("Fire1"))
+                    {
+                        FireBullet();
+                    }
+                    break;
+                case FireType.Automatic:
+                    if (mayFire == true)
+                    {
+                        FireBullet();
+                        StartCoroutine("Cooldown");
+                    }
+                    if (Input.GetButtonUp("Fire1"))
+                    {
+                        StopCoroutine("Cooldown");
+                        mayFire = true;
+                    }
+                    break;
+                case FireType.BoltOrPumpAction:
+                    if (mayFire == true)
+                    {
+                        FireBullet();
+                        StartCoroutine("Pump");
+                    }
+                    break;
+            }
         }
+        else if (mayFire == true && ammoInClip == 0)
+        {
+            switch (fireType)
+            {
+                case FireType.Automatic:
+                    audioSource.PlayOneShot(emptySound);
+                    StartCoroutine("Empty");
+                    break;
+                case FireType.SemiAutomatic:
+                    if (Input.GetButtonDown("Fire1"))
+                    {
+                        StartCoroutine("Empty");
+                        audioSource.PlayOneShot(emptySound);
+                    }
+                    break;
+                case FireType.BoltOrPumpAction:
+                    if (Input.GetButtonDown("Fire1"))
+                    {
+                        StartCoroutine("Empty");
+                        audioSource.PlayOneShot(emptySound);
+                    }
+                    break;
+            }
+        }
+
     }
 
     void FireBullet()
     {
+        audioSource.PlayOneShot(shot);
+        ammoInClip--;
         print("boom headshot");
     }
 
     IEnumerator Cooldown()
     {
-        mayFire=false;
+        mayFire = false;
         yield return new WaitForSeconds(fireRate);
-        mayFire=true;
+        mayFire = true;
     }
+
+    IEnumerator Empty()
+    {
+        print("StartCooldown");
+        mayFire = false;
+        yield return new WaitForSeconds(emptySound.length);
+        mayFire = true;
+        print(mayFire);
+    }
+
 
     IEnumerator Pump()
     {
-        yield return null;
+        mayFire = false;
+        yield return new WaitForSeconds(0.5f);
+        audioSource.PlayOneShot(pumpSound);
+        yield return new WaitForSeconds(pumpSound.length);
+        mayFire = true;
+    }
+
+    IEnumerator Reload()
+    {
+        mayFire=false;
+        audioSource.PlayOneShot(reloadSound);
+        yield return new WaitForSeconds(reloadSound.length);
+        mayFire=true;
+        ammoInClip=fullAmmoInClip;
     }
 }
---
+++ Development/FPS Project/Assets/Editor/GunScriptEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Gun))]
+public class GunScriptEditor : Editor
+{
+    
+
+    public override void OnInspectorGUI()
+    {
+        Gun myGunScript = (Gun)target;
+        myGunScript.fireType = (FireType)EditorGUILayout.EnumPopup("Fire Type", myGunScript.fireType);
+        switch (myGunScript.fireType)
+        {
+            case FireType.Automatic:
+                myGunScript.roundsPerMinute = EditorGUILayout.FloatField("Rounds per Minute", myGunScript.roundsPerMinute);
+                break;
+            case FireType.SemiAutomatic:
+                myGunScript.fireRate = EditorGUILayout.FloatField("Fire Rate", myGunScript.fireRate);
+                break;
+            case FireType.BoltOrPumpAction:
+                myGunScript.roundsPerMinute = EditorGUILayout.FloatField("Pump Time", myGunScript.pumpTime);
+                break;
+
+        }
+        myGunScript.ammoInClip = EditorGUILayout.IntField("Current Amount of Bullets", myGunScript.ammoInClip);
+        myGunScript.fullAmmoInClip = EditorGUILayout.IntField("Max Amount of Bullets", myGunScript.fullAmmoInClip);
+        myGunScript.reloadSpeed = EditorGUILayout.FloatField("Time spent Reloading", myGunScript.reloadSpeed);
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("EmptySound");
+        myGunScript.emptySound =  (AudioClip)  EditorGUILayout.ObjectField(myGunScript.emptySound, typeof(AudioClip) , true);
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("ShotSound");
+        myGunScript.shot = (AudioClip)EditorGUILayout.ObjectField(myGunScript.shot, typeof(AudioClip), true);
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("CockSound");
+        myGunScript.pumpSound = (AudioClip)EditorGUILayout.ObjectField(myGunScript.pumpSound, typeof(AudioClip), true);
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("ReloadSound");
+        myGunScript.reloadSound = (AudioClip)EditorGUILayout.ObjectField(myGunScript.reloadSound, typeof(AudioClip), true);
+        EditorGUILayout.EndHorizontal();
+
+    }
+}
