e5bc55b58e9d9e0312d6676b022df58a10133c02
RobinPC <97026403@st.deltion.nl>
2016-02-08 15:21:24 +0100

    Animated UI WIP
    Animated UI WIP

---
+++ Development/FPS Project/Assets/Assets/Scripts/Robin/MenuManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+	public GameObject rightCanvas;
+
+	public enum AllOptions {
+		StartGame,
+		Options,
+		Controls,
+		Credits,
+		MainMenu
+	}
+	public AllOptions Option;
+
+	void Awake () {
+		rightCanvas.SetActive(true);
+	}
+	
+	public void OnMouseDown () {
+		switch(Option){
+			case AllOptions.StartGame:
+				Debug.Log("StartGame");
+				StartGame();
+				break;
+			case AllOptions.Options:
+				Debug.Log("Options");
+				ShowOptions();
+				break;
+			case AllOptions.Controls:
+				Debug.Log("Controls");
+				ShowControls();
+				break;
+			case AllOptions.Credits:
+				Debug.Log("Credits");
+				ShowCredits();
+				break;
+			case AllOptions.MainMenu:
+				Debug.Log("Main Menu");
+				ReturnTomenu();
+				break;
+		}
+	}
+
+	void StartGame () {
+
+	}
+
+	void ShowOptions () {
+
+	}
+
+	void ShowControls () {
+
+	}
+
+	void ShowCredits () {
+
+	}
+
+	void ReturnTomenu () {
+
+	}
+}
