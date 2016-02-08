8f3d3dc5a887c8a17f0c06b9dff3f883f15323a0
ryuiji <superrief@hotmail.com>
2016-02-08 11:43:27 +0100

    KeySpawnBegin

---
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/KeyPartSpawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPartSpawn : MonoBehaviour {
+
+    public GameObject[] spawnKey;
+    public GameObject keyPart;
+    private int maxSpawn;
+
+	void Start () {
+
+        InvokeRepeating("RandomKeySpawn", 1, 1);
+
+    }
+    void RandomKeySpawn() {
+        while(maxSpawn < 3) {
+            Instantiate(keyPart, spawnKey[Random.Range(0, spawnKey.Length)].transform.position, Quaternion.identity);
+            maxSpawn++;
+        }
+
+    }
+
+    void Update() {
+
+    }
+}
