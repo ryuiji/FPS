ee9975ef64138d26e84a6832e9967a43996e1ac1
ryuiji <superrief@hotmail.com>
2016-02-09 15:31:15 +0100

    KeySpawn is nu decent, spawnen op 20 plekken
    kunnen soms nog op zelfde plekken spawen, moet k nog iets voor vinden

--- Development/FPS Project/Assets/Assets/Scripts/Rief/KeyPartSpawn.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/KeyPartSpawn.cs
@@ -7,17 +7,20 @@ public class KeyPartSpawn : MonoBehaviour {
     public GameObject keyPart;
     private int maxSpawn;
 
-	void Start () {
+    void Start () {
 
-        InvokeRepeating("RandomKeySpawn", 1, 1);
+        
+
+        InvokeRepeating("RandomKeySpawn", 0, 1);
 
     }
     void RandomKeySpawn() {
         while(maxSpawn < 3) {
             Instantiate(keyPart, spawnKey[Random.Range(0, spawnKey.Length)].transform.position, Quaternion.identity);
             maxSpawn++;
-        }
 
+        }
+        Destroy(gameObject);
     }
 
     void Update() {
