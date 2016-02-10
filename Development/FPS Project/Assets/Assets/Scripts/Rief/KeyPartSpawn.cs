c6ee046db0bd0ecf3cc91fa9b85241a0f8c243e5
ryuiji <superrief@hotmail.com>
2016-02-10 13:51:32 +0100

    Asset list update & keyspawn af

--- Development/FPS Project/Assets/Assets/Scripts/Rief/KeyPartSpawn.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/KeyPartSpawn.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class KeyPartSpawn : MonoBehaviour {
 
-    public GameObject[] spawnKey;
     public GameObject keyPart;
     private int maxSpawn;
+    public List<GameObject> keySpawns = new List<GameObject>();
 
     void Start () {
-
         
-
         InvokeRepeating("RandomKeySpawn", 0, 1);
 
     }
     void RandomKeySpawn() {
+
+
         while(maxSpawn < 3) {
-            Instantiate(keyPart, spawnKey[Random.Range(0, spawnKey.Length)].transform.position, Quaternion.identity);
+            int spawnNum = Random.Range(0, keySpawns.Count);
+            Instantiate(keyPart, keySpawns[spawnNum].transform.position, Quaternion.identity);
+            keySpawns.RemoveAt(spawnNum);
             maxSpawn++;
 
         }
