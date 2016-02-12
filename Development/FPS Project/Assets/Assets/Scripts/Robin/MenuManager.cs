c6cb30f377a45881c483d16593dff1e3f044aba0
RobinPC <97026403@st.deltion.nl>
2016-02-12 11:40:52 +0100

    Visio + MenuManager adjustments
    Visio + MenuManager adjustments

--- Development/FPS Project/Assets/Assets/Scripts/Robin/MenuManager.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Robin/MenuManager.cs
@@ -5,9 +5,7 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour {
 	public GameObject leftCanvas;
 	public GameObject rightCanvas;
-	public GameObject optionMenu;
-	public bool isOn = true;
-	private bool isFading;
+	public bool showMenu = true;
 
 	public enum AllOptions {
 		StartGame,
@@ -20,10 +18,7 @@ public class MenuManager : MonoBehaviour {
 
 	void GetInput () {
 		if(Input.GetButtonDown("Cancel")){
-			leftCanvas.SetActive(isOn);
-			rightCanvas.SetActive(isOn);
-			isOn = !isOn;
-			Debug.Log ("De functie GetInput wordt uitgevoerd.");
+			ShowMenu();
 		}
 	}
 
@@ -31,61 +26,38 @@ public class MenuManager : MonoBehaviour {
 		switch(Option){
 			case AllOptions.StartGame:
 				Debug.Log("StartGame");
-				Resume();
+				HideMenu();
 				break;
 			case AllOptions.Options:
 				Debug.Log("Options");
-				ShowOptions();
-				break;
+				//
+				break;	
 			case AllOptions.Controls:
 				Debug.Log("Controls");
-				ShowControls();
+				//
 				break;
 			case AllOptions.Credits:
 				Debug.Log("Credits");
-				ShowCredits();
+				//
 				break;
 			case AllOptions.MainMenu:
 				Debug.Log("Main Menu");
-				ReturnTomenu();
+				//
 				break;
 		}
 	}
 
+	void ShowMenu () {
+		leftCanvas.SetActive(true);
+		rightCanvas.SetActive(true);
+	}
+
+	void HideMenu () {
+		leftCanvas.SetActive(false);
+		rightCanvas.SetActive(false);
+	}
+
 	void Update () {
 		GetInput();
-		print(isOn);
-	}
-	/*
-		
-	*/
-
-	void Resume () {
-		isOn = true;
-		leftCanvas.SetActive(!isOn);
-		optionMenu.SetActive(!isOn);
-		Debug.Log ("De functie Resume wordt uitgevoerd. Dit moet isOn op true zetten.");
-	}
-
-	void ShowOptions () {
-		optionMenu.SetActive(isOn);
-		isOn = !isOn;
-		Debug.Log ("De functie ShowOptions wordt uitgevoerd.");
-	}
-
-	void ShowControls () {
-		
-	}
-
-	void ShowCredits () {
-
-	}
-
-	void ReturnTomenu () {
-
-	}
-
-	public void Back () {
-		optionMenu.SetActive(false);
 	}
 }
