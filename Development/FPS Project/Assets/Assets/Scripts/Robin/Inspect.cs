ad4a5bfb4165b57548753ad3af01aa41b3411adf
RobinPC <97026403@st.deltion.nl>
2016-02-22 13:59:24 +0100

    Prefabs + Inspect script WIP

---
+++ Development/FPS Project/Assets/Assets/Scripts/Robin/Inspect.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inspect : MonoBehaviour {
+	public float rayDistance;
+	private GameObject inspectObject;
+	private bool inspecting;
+	private RaycastHit hit;
+	
+	void Update () {
+		CheckBool();
+	}
+
+	void CheckBool () {
+		if(inspecting == false){
+			ShootRay();
+		}else{
+			InspectObject();
+		}
+	}
+
+	void ShootRay () {
+		Debug.DrawRay(transform.position, transform.forward, Color.red);
+		if(Physics.Raycast(transform.position, transform.forward, out hit, rayDistance)){
+			CheckTag(hit);
+		}
+	}
+
+	void CheckTag (RaycastHit hit) {
+		if(hit.transform.tag == "Inspect"){
+			Debug.Log("Hit");
+			if(Input.GetButtonDown("Use")){
+				inspectObject = hit.transform.gameObject;
+				inspecting = true;
+			}
+		}
+	}
+
+	void InspectObject() {
+
+	}
+}
