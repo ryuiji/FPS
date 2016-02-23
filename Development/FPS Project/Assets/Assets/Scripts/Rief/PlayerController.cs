5c8a93ff86add1373db6642683398f087eed3a35
ryuiji <superrief@hotmail.com>
2016-02-23 11:56:19 +0100

    player controll begin

---
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float moveSpeed;
+    public float rotationSpeed;
+
+
+    private bool mayMove;
+    public Vector3 moveDirection;
+
+    void Start() {
+        mayMove = true;
+    }
+    void FixedUpdate() {
+        moveDirection = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, 1)) {
+            mayMove = false;
+        }
+        if(mayMove == true) {
+        }
+    }
+}
