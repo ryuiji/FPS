7ef6131d93f19b7109a930eb1c2ff88d5a027581
ryuiji <superrief@hotmail.com>
2016-02-28 13:52:18 +0100

    player controller changes

--- Development/FPS Project/Assets/Assets/Scripts/Rief/PlayerController.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/PlayerController.cs
@@ -3,19 +3,38 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-
-    public float moveSpeed;
-    public float straveSpeed;
     public CharacterController cc;
+    public GameObject mainCamera;
+    private Vector3 movement;
+    private float moveSpeed = 4f;
+    private float rotateSpeed = 4f;
+    private float jumpForce = 750f;
+    private float gravity = 10f;
+    public Vector3 tempRotation;
 
     void Start() {
         cc = GetComponent<CharacterController>();
     }
-    void FixedUpdate() {
+    void Update() {
+        float vertical = rotateSpeed * -Input.GetAxis("Mouse Y");
+        float horizontal = rotateSpeed * Input.GetAxis("Mouse X");
+        transform.Rotate(0, horizontal, 0);
+        mainCamera.transform.Rotate(vertical, 0, 0);
+        Jump();
+    }
 
-        Vector3 moveForward = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * moveSpeed;
-        Vector3 moveSide = Input.GetAxis("Horizontal") * transform.TransformDirection(Vector3.right) * straveSpeed;
-        cc.Move(moveForward * Time.deltaTime);
-        cc.Move(moveSide * Time.deltaTime);
+    void FixedUpdate() {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movement *= moveSpeed * Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        cc.Move(movement);
+
+    }
+    void Jump() {
+        if (Input.GetButtonDown ("Jump") && cc.isGrounded) {
+            movement.y = jumpForce * Time.deltaTime;
+        }
+        movement.y -= gravity * Time.deltaTime;
+        cc.Move(movement * Time.deltaTime);
     }
 }
