6983f8165334d673c7885df1e36e0198fef5e7e3
ryuiji <superrief@hotmail.com>
2016-03-10 11:45:07 +0100

    Camera blur, mouse lock & crosshair lookat

---
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/CursorLookAt.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorLookAt : MonoBehaviour {
+    
+	void Start () {
+	
+	}
+	
+	void Update () {
+        transform.LookAt(Camera.main.transform.position);
+    }
+}
--- Development/FPS Project/Assets/Assets/Scripts/Rief/PlayerController.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/PlayerController.cs
@@ -1,31 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.ImageEffects;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public CharacterController cc;
-    public GameObject mainCamera;
     private Vector3 movement;
     private float moveSpeed = 4f;
     private float rotateSpeed = 4f;
     private float jumpForce = 750f;
     private float gravity = 10f;
     public Vector3 tempRotation;
-
     public float curRot;
 
     void Start() {
         cc = GetComponent<CharacterController>();
     }
+
     void Update() {
-        float vertical = rotateSpeed * -Input.GetAxis("Mouse Y");
-        float horizontal = rotateSpeed * Input.GetAxis("Mouse X");
-        transform.Rotate(0, horizontal, 0);
-        mainCamera.transform.Rotate(vertical, 0, 0);
-        curRot += vertical;
-        curRot = Mathf.Clamp(curRot, 0, 30); //aanpassen wanneer we de main character hebben.
-        mainCamera.transform.localEulerAngles = new Vector3(curRot, mainCamera.transform.localEulerAngles.y, mainCamera.transform.localEulerAngles.z);
+        MouseMoveClamp();
         Jump();
     }
 
@@ -34,8 +28,18 @@ public class PlayerController : MonoBehaviour {
         movement *= moveSpeed * Time.deltaTime;
         movement = transform.TransformDirection(movement);
         cc.Move(movement);
-
     }
+
+    void MouseMoveClamp() {
+        float vertical = rotateSpeed * -Input.GetAxis("Mouse Y");
+        float horizontal = rotateSpeed * Input.GetAxis("Mouse X");
+        transform.Rotate(0, horizontal, 0);
+        Camera.main.transform.Rotate(vertical, 0, 0);
+        curRot += vertical;
+        curRot = Mathf.Clamp(curRot, 0, 30); //aanpassen wanneer we de main character hebben. (1e naar boven, 2e naar onder)
+        Camera.main.transform.localEulerAngles = new Vector3(curRot, Camera.main.transform.localEulerAngles.y, Camera.main.transform.localEulerAngles.z);
+    }
+
     void Jump() {
         if (Input.GetButtonDown ("Jump") && cc.isGrounded) {
             movement.y = jumpForce * Time.deltaTime;
