4c5b384ee2d0dbe3d4cb21caf7427633d22513b3
Zinedine038 <zsghari@hotmail.com>
2016-03-14 09:50:43 +0100

    Hope this goes well

---
+++ Development/FPS Project/Assets/Assets/Scripts/Zine/EnemyAbstract.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class EnemyAbstract : MonoBehaviour 
+{
+	public int health;
+	public int damage;
+	public float moveSpeed;
+	public bool lineOfSight;
+	public float lengthOfSight;
+	public NavMeshAgent agent;
+
+	public abstract void Aggro();
+	public abstract void DeAggro();
+	public abstract void Chase();
+	public abstract void Roam();
+	public abstract void Attack();
+	public abstract void TakeDamage(int damage);
+	public abstract IEnumerator Death();
+}
