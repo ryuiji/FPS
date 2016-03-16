fc18ae224e009677a04dec1db64e2d70bc993c5b
RobinPC <97026403@st.deltion.nl>
2016-03-16 19:02:08 +0100

    Inspect WIP
    Inspect WIP

--- Development/FPS Project/Assets/Assets/Scripts/Robin/Inspect.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Robin/Inspect.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Inspect : MonoBehaviour {
 	public float rayDistance;
-	private GameObject inspectObject;
+	public GameObject inspectText;
+	public GameObject inspectCanvas;
 	private bool inspecting;
-	private RaycastHit hit;
+	public RaycastHit hit;
+
+	public GameObject name;
+	public GameObject weight;
+	public GameObject tip;
+	public GameObject description;
 	
 	void Update () {
 		CheckBool();
@@ -14,29 +21,43 @@ public class Inspect : MonoBehaviour {
 	void CheckBool () {
 		if(inspecting == false){
 			ShootRay();
-		}else{
-			InspectObject();
 		}
 	}
 
 	void ShootRay () {
-		Debug.DrawRay(transform.position, transform.forward, Color.red);
 		if(Physics.Raycast(transform.position, transform.forward, out hit, rayDistance)){
 			CheckTag(hit);
+		}else{
+			inspectText.SetActive(false);
 		}
 	}
 
 	void CheckTag (RaycastHit hit) {
-		if(hit.transform.tag == "Inspect"){
-			Debug.Log("Hit");
-			if(Input.GetButtonDown("Use")){
-				inspectObject = hit.transform.gameObject;
+		if(hit.transform.tag == "Inspect") {
+			inspectText.SetActive(true);
+			if(Input.GetButtonDown("Use")) {
 				inspecting = true;
+				InspectObject();
+				inspectText.SetActive(false);
 			}
 		}
 	}
 
 	void InspectObject() {
+		DisableMovement();
+		GetInfo();
+		inspectCanvas.SetActive(true);
+	}
 
+	void GetInfo () {
+		ItemStats itemStats = hit.transform.gameObject.GetComponent<ItemStats>();
+		name.GetComponent<Text>().text = itemStats.name;
+		weight.GetComponent<Text>().text = itemStats.weight.ToString();
+		tip.GetComponent<Text>().text = itemStats.tip;
+		description.GetComponent<Text>().text = itemStats.description;
+	}
+
+	void DisableMovement () {
+		//
 	}
 }
