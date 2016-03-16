7df030bbf369340f882824c946e3dca8406befc5
ryuiji <superrief@hotmail.com>
2016-03-16 16:30:51 +0100

    CloseDoor & usescript bezig

---
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/UseScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UseScript : MonoBehaviour {
+
+    public RaycastHit hit;
+    public float rayDis;
+
+    void Start() {
+    }
+
+    void Update () {
+        OpenDoor();
+	}
+
+    void OpenDoor() {
+        if (Input.GetButtonDown("Use")){
+            if (Physics.Raycast(transform.position, transform.forward, out hit, rayDis) && hit.transform.tag == ("Deur")) {
+                hit.transform.GetComponent<Animator>().SetTrigger("InteractDoor");
+            }
+        }
+    }
+}
