035073c31334a112636a330815c3340310b2cd32
Zinedine038 <zsghari@hotmail.com>
2016-03-17 13:31:52 +0100

    COMMIT BBY

---
+++ Development/FPS Project/Assets/Assets/Scripts/Zine/RunnerEnemy.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunnerEnemy : EnemyAbstract
+{
+    public AudioClip screamSound;
+    public float sprintSpeed;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = moveSpeed;
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        CheckForPlayer();
+    }
+
+    public override void CheckForPlayer()
+    {
+        if (aggrod == false)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) < lengthOfSight)
+            {
+                if (CheckLineOfSight() == true)
+                {
+                    print("Engaged");
+                    Aggro();
+                }
+            }
+        }
+        else if (aggrod == true)
+        {
+            agent.SetDestination(player.transform.position);
+            if (Vector3.Distance(transform.position, player.transform.position) < attackRange)
+            {
+                if (isAttacking == false)
+                {
+                    StartCoroutine("Attack");
+                }
+                agent.Stop();
+            }
+            if (Vector3.Distance(transform.position, player.transform.position) > attackRange && Vector3.Distance(transform.position, player.transform.position) < lengthOfSight)
+            {
+                StopCoroutine("Attack");
+                agent.SetDestination(player.transform.position);
+                agent.Resume();
+            }
+            if (Vector3.Distance(transform.position, player.transform.position) > lengthOfSight)
+            {
+                DeAggro();
+            }
+        }
+
+    }
+
+    public override void Aggro()
+    {
+        aggrod = true;
+        agent.SetDestination(player.transform.position);
+        agent.Resume();
+        StartCoroutine("Charge");
+    }
+
+    public IEnumerator Charge()
+    {
+        agent.speed=0;
+        audioSource.PlayOneShot(screamSound);
+        yield return new WaitForSeconds(1);
+        agent.speed=sprintSpeed;
+    }
+
+    public override IEnumerator Attack()
+    {
+        isAttacking = true;
+        player.GetComponent<MakeShiftHp>().TakeDamage(damage);
+        yield return new WaitForSeconds(timeBetweenAttacks);
+        StartCoroutine("Attack");
+    }
+
+    public override void Chase()
+    {
+        //play walk anim
+    }
+
+    public override void DeAggro()
+    {
+        aggrod = false;
+        agent.Stop();
+    }
+
+    public override IEnumerator Death()
+    {
+        //play anim
+        yield return new WaitForSeconds(0.1f);  //anim length
+        Destroy(this.gameObject);
+    }
+
+    public override void Roam()
+    {
+        //NYI
+    }
+
+
+    public override void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            StartCoroutine("Death");
+        }
+    }
+
+    public override bool CheckLineOfSight()
+    {
+        if (Physics.Linecast(transform.position, player.transform.position, out hit))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Development/FPS Project/Assets/Assets/Scripts/Zine/TestFiles/MakeShiftHp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeShiftHp : MonoBehaviour
+{
+    public int hp;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int damageTaken)
+    {
+        hp-=damageTaken;
+        if(hp<=0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Destroy(this.gameObject);
+    }
+}
