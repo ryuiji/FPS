638f12b2616c0b1e320efa8bf85bb75839d14b16
ryuiji <superrief@hotmail.com>
2016-04-04 09:58:05 +0200

    fix

--- Development/FPS Project/Assets/Assets/Scripts/Robin/ItemStats.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ItemStats : MonoBehaviour {
-	public string name;
-	public float weight;
-	public string tip;
-	public string description;
-}
